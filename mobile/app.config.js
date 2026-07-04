7e914154a96c37e19846e8da5f3989dc13fce4c9
Echezona <echezonaomale2@gmail.com>
2026-07-04 22:59:44 +0100

    chore: bump mobile to v1.0.2 (build 3) for final production APK

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -36,7 +36,7 @@ if (!firebaseConfig.isConfigured) {
 module.exports = {
   name: 'Pingload',
   slug: 'pingload',
-  version: '1.0.1',
+  version: '1.0.2',
   orientation: 'portrait',
   icon: './src/assets/icon.png',
   userInterfaceStyle: 'automatic',
@@ -64,7 +64,7 @@ module.exports = {
   },
   android: {
     package: 'com.pingload.app',
-    versionCode: 2,
+    versionCode: 3,
     icon: './src/assets/icon.png',
     googleServicesFile: firebaseConfig.androidConfigPath,
     adaptiveIcon: {
