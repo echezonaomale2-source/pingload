51bbbe0c558daf4c95004886c681164fa4531a0b
Echezona <echezonaomale2@gmail.com>
2026-07-05 13:43:33 +0100

    chore(mobile): bump version to 1.0.5 build 6 for production APK

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -36,7 +36,7 @@ if (!firebaseConfig.isConfigured) {
 module.exports = {
   name: 'Pingload',
   slug: 'pingload',
-  version: '1.0.4',
+  version: '1.0.5',
   orientation: 'portrait',
   icon: './src/assets/icon.png',
   userInterfaceStyle: 'automatic',
@@ -64,7 +64,7 @@ module.exports = {
   },
   android: {
     package: 'com.pingload.app',
-    versionCode: 5,
+    versionCode: 6,
     icon: './src/assets/icon.png',
     googleServicesFile: firebaseConfig.androidConfigPath,
     adaptiveIcon: {
